ec19c0ec1344495e81e2b09106fb6a01cea53944
Coverage <67969086+CoverageCraft@users.noreply.github.com>
2021-02-20 16:42:19 +0400

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '5085b416926ae24765445ba62b76676d';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 25.204849;
+  let longitude = 55.270782;
 
   getWeather(latitude, longitude);
 }
